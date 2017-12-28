d450ca2fe5c947e27142f6f9152641bafb745b9d
Meelis Talvis <meelis.talvis@derivco.ee>
2017-12-28 17:42:12 +0200

    Added Autofac as an Inversion of Control container

--- FrontEndWeb/Client/Global.asax.cs
+++ FrontEndWeb/Client/Global.asax.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Autofac;
+using Autofac.Integration.Mvc;
 
 namespace Client
 {
@@ -16,6 +18,32 @@ namespace Client
       FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
       RouteConfig.RegisterRoutes(RouteTable.Routes);
       BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+      var builder = new ContainerBuilder();
+
+      // Register your MVC controllers. (MvcApplication is the name of
+      // the class in Global.asax.)
+      builder.RegisterControllers(typeof(MvcApplication).Assembly);
+
+      // OPTIONAL: Register model binders that require DI.
+      builder.RegisterModelBinders(typeof(MvcApplication).Assembly);
+      builder.RegisterModelBinderProvider();
+
+      // OPTIONAL: Register web abstractions like HttpContextBase.
+      builder.RegisterModule<AutofacWebTypesModule>();
+
+      // OPTIONAL: Enable property injection in view pages.
+      builder.RegisterSource(new ViewRegistrationSource());
+
+      // OPTIONAL: Enable property injection into action filters.
+      builder.RegisterFilterProvider();
+
+      // OPTIONAL: Enable action method parameter injection (RARE).
+      //builder.InjectActionInvoker();
+
+      // Set the dependency resolver to be Autofac.
+      var container = builder.Build();
+      DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
     }
   }
 }
